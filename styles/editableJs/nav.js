a714f0c8bb0d4f21e73dfec7993360346cb7b693
aao-hd-repo <eyacorkett4@gmail.com>
2026-08-15 23:51:37 +0800

    Add link to the website's code in nav.js

--- styles/editableJs/nav.js
+++ styles/editableJs/nav.js
@@ -16,6 +16,8 @@ if (nativeNav) {
 			<li><a href="/Scripts/">AAO Website Scripts</a></li>
 			<br><br><br>
 			<li><a href="https://aaonline.fr">Play the tutorial in AAO!</a></li>
+			<br><br><br>
+			<li><a href="https://github.com/aao-hd-repo/aao-hd-repo.github.io">See the website's code</a></li>
 		</ul>
 	`;
 }
